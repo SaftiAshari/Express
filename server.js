7e9361db724e39e9ff539871fd5d9a26940ba526
SaftiAshari <ashari1905107@stud.kuet.ac.bd>
2023-09-23 11:26:58 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port=8000;
+// app.use(express.static('public'))
+// const validateUser=(req,res,next)=>{
+    // res.locals.validated=true;
+    // console.log("1","validated");
+    // next();
+// }
+// app.use("/",validateUser);
+// app.get("/",(req,res,next)=>{
+    // res.send("<h1>Main Page</h1>");
+    // console.log(2,res.locals.validated);
+// 
+// });
+
+app.get("/",(req,res,next)=>{
+    res.redirect("/welcome");
+});
+app.get("/welcome",(req,res,next)=>{
+    res.json({message:"Welcome"});
+});
+
+app.listen(8000, () => console.log(`Server started on ${port}`));
